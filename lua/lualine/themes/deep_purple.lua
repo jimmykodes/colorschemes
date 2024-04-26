35150d44af60d3a62127a6564c0289764fa5ea5a
jimmykodes <jimmy@khanacademy.org>
2024-04-26 14:37:32 -0700

    feat: add deep purple

---
+++ lua/lualine/themes/deep_purple.lua
@@ -0,0 +1,29 @@
+local colors = {
+	normal  = '#535C91',
+	insert  = '#6eaa76',
+	visual  = '#1B1A55',
+	replace = '#f15d22',
+	command = '#d19a66',
+
+	fg      = '#9290C3',
+	bg      = '#000017',
+	bg_alt  = '#000000',
+}
+
+return {
+	normal = {
+		a = { fg = colors.bg, bg = colors.normal },
+		b = { fg = colors.normal, bg = colors.bg },
+		c = { fg = colors.fg, bg = colors.bg_alt },
+	},
+	insert = { a = { fg = colors.bg, bg = colors.insert } },
+	visual = { a = { fg = colors.bg, bg = colors.visual } },
+	command = { a = { fg = colors.bg, bg = colors.command } },
+	replace = { a = { fg = colors.bg, bg = colors.replace } },
+
+	inactive = {
+		a = { bg = colors.bg, fg = colors.normal },
+		b = { bg = colors.bg, fg = colors.bg_alt },
+		c = { bg = colors.bg, fg = colors.bg_alt },
+	},
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"text/template"
 
@@ -104,6 +105,143 @@ func (h *Highlight) UnmarshalText(text []byte) error {
 	return nil
 }
 
+type Color struct {
+	value string
+	f     string
+	args  []string
+}
+
+func (c Color) String() string {
+	return c.value
+}
+
+func (c Color) R(colors map[string]*Color) (int64, error) {
+	val, err := c.Value(colors)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(val[1:3], 16, 64)
+}
+
+func (c Color) G(colors map[string]*Color) (int64, error) {
+	val, err := c.Value(colors)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(val[3:5], 16, 64)
+}
+
+func (c Color) B(colors map[string]*Color) (int64, error) {
+	val, err := c.Value(colors)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(val[5:], 16, 64)
+}
+
+func (c *Color) UnmarshalText(text []byte) error {
+	if text[0] == '#' {
+		c.value = string(text)
+		return nil
+	}
+	f, argbytes, found := bytes.Cut(text, []byte{' '})
+	if !found {
+		return fmt.Errorf("invalid data - must provide a hex color or a function and at least one arg")
+	}
+	c.f = string(f)
+	c.args = strings.Split(string(argbytes), " ")
+	return nil
+}
+
+func clamp(val, lower, upper int64) int64 {
+	return max(min(val, upper), lower)
+}
+
+func rgbClamp(val int64) int64 {
+	return clamp(val, 0, 255)
+}
+
+type _funcs struct{}
+
+var funcs = _funcs{}
+
+func (f _funcs) darken(colors map[string]*Color, c string, amount int64) (string, error) {
+	col, ok := colors[c]
+	if !ok {
+		return "", fmt.Errorf("invalid color %s", c)
+	}
+
+	r, err := col.R(colors)
+	if err != nil {
+		return "", err
+	}
+	g, err := col.G(colors)
+	if err != nil {
+		return "", err
+	}
+	b, err := col.B(colors)
+	if err != nil {
+		return "", err
+	}
+
+	r = rgbClamp(r - amount)
+	g = rgbClamp(g - amount)
+	b = rgbClamp(b - amount)
+
+	return fmt.Sprintf("#%02x%02x%02x", r, g, b), nil
+}
+
+func (f _funcs) lighten(colors map[string]*Color, c string, amount int64) (string, error) {
+	return f.darken(colors, c, -amount)
+}
+
+func (f _funcs) ref(colors map[string]*Color, c string) (string, error) {
+	col, ok := colors[c]
+	if !ok {
+		return "", fmt.Errorf("invalid color %s", c)
+	}
+	return col.Value(colors)
+}
+
+func (c *Color) Value(colors map[string]*Color) (string, error) {
+	if c.value != "" {
+		return c.value, nil
+	}
+	switch c.f {
+	case "darken":
+		amount, err := strconv.ParseInt(c.args[1], 10, 64)
+		if err != nil {
+			return "", err
+		}
+		val, err := funcs.darken(colors, c.args[0], amount)
+		if err != nil {
+			return "", err
+		}
+		c.value = val
+		return val, nil
+	case "lighten":
+		amount, err := strconv.ParseInt(c.args[1], 10, 64)
+		if err != nil {
+			return "", err
+		}
+		val, err := funcs.lighten(colors, c.args[0], amount)
+		if err != nil {
+			return "", err
+		}
+		c.value = val
+		return val, nil
+	case "ref":
+		val, err := funcs.ref(colors, c.args[0])
+		if err != nil {
+			return "", err
+		}
+		c.value = val
+		return val, nil
+	default:
+		return "", fmt.Errorf("invalid function %s", c.f)
+	}
+}
+
 type Scheme struct {
 	Metadata struct {
 		Name  string `yaml:"name"`
@@ -120,7 +258,7 @@ type Scheme struct {
 		BgAlt   string `yaml:"bg_alt"`
 	} `yaml:"lualine"`
 	Normal Highlight                       `yaml:"Normal"`
-	Colors map[string]string               `yaml:"colors"`
+	Colors map[string]*Color               `yaml:"colors"`
 	Groups map[string]map[string]Highlight `yaml:"groups"`
 }
 
@@ -136,6 +274,14 @@ func genScheme(schemeTemplate SchemeTemplate) error {
 	if err := yaml.NewDecoder(f).Decode(&scheme); err != nil {
 		return err
 	}
+
+	for _, color := range scheme.Colors {
+		_, err := color.Value(scheme.Colors)
+		if err != nil {
+			return err
+		}
+	}
+
 	colorDir := "colors"
 	if err := os.MkdirAll(colorDir, 0766); err != nil && !os.IsExist(err) {
 		return err
@@ -277,7 +423,8 @@ return M
 `
 
 	colorsTmpl = `local M = {}
-{{ range $name, $color := .Colors }}M.{{$name}} = '{{ $color }}'
+{{- $colors := .Colors }}
+{{ range $name, $color := .Colors }}M.{{$name}} = '{{ $color.Value $colors }}'
 {{end}}
 return M
 `
