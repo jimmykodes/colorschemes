13cdd781d1d6b6cd8aa3673102d620163071e19e
jimmykodes <jimmy@khanacademy.org>
2026-04-03 11:35:36 -0700

    feat: add pokemon

--- schemes/all.go
+++ schemes/all.go
@@ -5,10 +5,12 @@ import (
 	"github.com/jimmykodes/colorschemes/schemes/arctic"
 	"github.com/jimmykodes/colorschemes/schemes/black_hole"
 	"github.com/jimmykodes/colorschemes/schemes/deep_purple"
+	"github.com/jimmykodes/colorschemes/schemes/pokemon"
 )
 
 var All = []*colorschemes.Colorscheme{
 	arctic.Arctic,
 	black_hole.BlackHole,
 	deep_purple.DeepPurple,
+	pokemon.Pokemon,
 }
