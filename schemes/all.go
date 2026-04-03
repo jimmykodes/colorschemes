133dc9c036062df15c36b0b32bf7ca19bdb516a5
jimmykodes <jimmy@khanacademy.org>
2026-04-03 11:22:49 -0700

    feat: add blackhole

--- schemes/all.go
+++ schemes/all.go
@@ -3,8 +3,10 @@ package schemes
 import (
 	"github.com/jimmykodes/colorschemes"
 	"github.com/jimmykodes/colorschemes/schemes/arctic"
+	"github.com/jimmykodes/colorschemes/schemes/black_hole"
 )
 
 var All = []*colorschemes.Colorscheme{
 	arctic.Arctic,
+	black_hole.BlackHole,
 }
