abbeb90292ead6f0ad987654258a478f4b569e9f
jimmykodes <jimmy@khanacademy.org>
2026-04-20 11:24:07 -0700

    fix: do not clear highlights

--- lua/arctic/init.lua
+++ lua/arctic/init.lua
@@ -2,8 +2,6 @@ local M = {}
 local theme = require("arctic.theme")
 
 function M.setup()
-	vim.cmd('hi clear')
-
 	vim.o.background = 'dark'
 	if vim.fn.exists('syntax_on') then
 		vim.cmd('syntax reset')
--- lua/system76/init.lua
+++ lua/system76/init.lua
@@ -2,8 +2,6 @@ local M = {}
 local theme = require("system76.theme")
 
 function M.setup()
-	vim.cmd('hi clear')
-
 	vim.o.background = 'dark'
 	if vim.fn.exists('syntax_on') then
 		vim.cmd('syntax reset')
